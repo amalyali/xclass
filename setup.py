7b25cd988a95318ce90c2cd657b6ab9ceaee1f78
Adam Malyali <adammalyali@gmail.com>
2024-01-05 09:51:59 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='xclass',
+      version='1.0',
+      author='Adam Malyali',
+      description='Python package for X-ray source classification',
+      url='https://github.com/amalyali/xclass/',
+      packages=['xclass']
+)
